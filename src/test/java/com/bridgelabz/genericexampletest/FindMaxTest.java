c75be5b5498984c8d9b339bb938731ecae00e379
“Shubham” <shubhampattar2@gmail.com>
2020-06-21 17:24:52 +0530

    ADDED find max from more than 3 values

--- src/test/java/com/bridgelabz/genericexampletest/FindMaxTest.java
+++ src/test/java/com/bridgelabz/genericexampletest/FindMaxTest.java
@@ -4,14 +4,15 @@ import com.bridgelabz.genericexample.FindMax;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /*
  * These Test Cases will Test for FindMax class
  * To check Whether it is working Fine/Not and returning correct values
  * These Test Cases are providing 3 parameters to the method to find largest of 3 numbers
  */
 public class FindMaxTest {
-    private FindMax findMax;
-
     @Test
     public void givenFirstIntegerNumberAsLarge_ShouldReturn_FirstNumberAsMax() {
         Integer[] integerValues = {200, 15, 100};
@@ -49,7 +50,7 @@ public class FindMaxTest {
 
     @Test
     public void givenThirdFloatNumberAsLarge_ShouldReturn_ThirdNumberAsMax() {
-        Float[] floatValues = {200f, 15f, 1000f};
+        Float[] floatValues = {200F, 15F, 1000F};
         Float max = new FindMax<>(floatValues).findMaxValue();
         Assert.assertEquals((Float) 1000f, max);
     }
@@ -73,5 +74,38 @@ public class FindMaxTest {
         String[] stringValues = {"All", "Good", "Morning"};
         String max = new FindMax<>(stringValues).findMaxValue();
         Assert.assertEquals("Morning", max);
+    }
+
+    @Test
+    public void givenListOfIntegers_ShouldReturn_MaxValue() {
+        List<Integer> integerList = new LinkedList<>();
+        integerList.add(50);
+        integerList.add(60);
+        integerList.add(20);
+        integerList.add(80);
+        Object max = new FindMax<>(integerList).findMax();
+        Assert.assertEquals(80,max);
+    }
+
+    @Test
+    public void givenListOfFloats_ShouldReturn_MaxValue() {
+        List<Float> floatList = new LinkedList<>();
+        floatList.add(50f);
+        floatList.add(60f);
+        floatList.add(20f);
+        floatList.add(80f);
+        Object max = new FindMax<>(floatList).findMax();
+        Assert.assertEquals(80f,max);
+    }
+
+    @Test
+    public void givenListOfStrings_ShouldReturn_MaxValue() {
+        List<String> stringList = new LinkedList<>();
+        stringList.add("Good");
+        stringList.add("Morning");
+        stringList.add("All");
+        stringList.add("To");
+        Object max = new FindMax<>(stringList).findMax();
+        Assert.assertEquals("To",max);
     }
 }
