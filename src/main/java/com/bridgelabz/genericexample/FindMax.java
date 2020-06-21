2f8f77f5bf234fb57f3a8ebc32ce19269efb43b8
“Shubham” <shubhampattar2@gmail.com>
2020-06-21 18:02:09 +0530

    ADDED Print Max Value Method

--- src/main/java/com/bridgelabz/genericexample/FindMax.java
+++ src/main/java/com/bridgelabz/genericexample/FindMax.java
@@ -25,18 +25,31 @@ public class FindMax<E extends Comparable<E>> {
 
     //Method to Find  Max Value From List
     public E findMax() {
-        return Collections.max(listValues);
+        E max = Collections.max(listValues);
+        printMax(max);
+        return max;
     }
 
     //Method to Find Max Value Form Array
     public E findMaxValue() {
         if (values[0].compareTo(values[1]) > 0 && values[0].compareTo(values[2]) > 0) {
+            printMax(values[0]);
             return values[0];
         } else if (values[1].compareTo(values[0]) > 0 && values[1].compareTo(values[2]) > 0) {
+            printMax(values[1]);
             return values[1];
         } else {
+            printMax(values[2]);
             return values[2];
         }
+    }
+
+    /*
+     *Method to print Max Value
+     * @param is Generic Value
+     */
+    private void printMax(E value) {
+        System.out.println("Max Value = " + value);
     }
 
 }
