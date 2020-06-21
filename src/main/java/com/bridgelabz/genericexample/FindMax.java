c140fd20fe66f573e8ff42595729545126337323
“Shubham” <shubhampattar2@gmail.com>
2020-06-21 17:50:56 +0530

    REFACTORED for Code Hygiene and Proper Comments

--- src/main/java/com/bridgelabz/genericexample/FindMax.java
+++ src/main/java/com/bridgelabz/genericexample/FindMax.java
@@ -3,24 +3,32 @@ package com.bridgelabz.genericexample;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
 
+/*
+ * Generic Class To Find Max Values
+ */
 public class FindMax<E extends Comparable<E>> {
+    //Array of Generic values
     E[] values;
+    //List of Generic Values
     List<E> listValues = new LinkedList<>();
 
+    //Constructor to Initialize Generic Array
     public FindMax(E[] values) {
         this.values = values;
     }
 
+    //Constructor to Initialize Generic List
     public FindMax(List<E> listValues) {
         this.listValues = listValues;
     }
 
-    public <E> E findMax() {
-        return (E) Collections.max(listValues);
+    //Method to Find  Max Value From List
+    public E findMax() {
+        return Collections.max(listValues);
     }
 
+    //Method to Find Max Value Form Array
     public E findMaxValue() {
         if (values[0].compareTo(values[1]) > 0 && values[0].compareTo(values[2]) > 0) {
             return values[0];
